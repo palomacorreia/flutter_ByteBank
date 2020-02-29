6f38384d3fdf6f08fd666dd9434c6a0467a3dac5
Paloma <palomacorreia>
2020-02-28 22:36:04 -0300

    project structure

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() =>
+    runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Transferências"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+          body: ListaTranferencias()),
+    ));
+
+class ListaTranferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(1000, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numConta.toString()),
+        ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numConta;
+
+  Transferencia(this.valor, this.numConta);
+}
