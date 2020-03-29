6807fbff79e3ebf643c5f4843bb19e46cad15866
Paloma <palomacorreiadasilva@gmail.com>
2020-03-29 20:07:25 -0300

    create Form transfers

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(BytebankApp());
+void main() => runApp(BytebankApp());
 
 class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: FormularioTransferencias ()
-      ),
+      home: Scaffold(body: FormularioTransferencias()),
     );
   }
 }
 
+class FormularioTransferencias extends StatelessWidget {
+  final TextEditingController _controllerFieldNumberAccount =
+      TextEditingController();
+  final TextEditingController _controllerFieldValue = TextEditingController();
 
-class FormularioTransferencias  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Criando Transferências"),),
+          title: Text("Criando Transferências"),
+        ),
         body: Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
-                style: TextStyle(
-                  fontSize: 24.0
-                ),
+                controller: _controllerFieldNumberAccount,
+                style: TextStyle(fontSize: 24.0),
                 decoration: InputDecoration(
-                  labelText: "Número da Conta",
-                  hintText: "0000"
-                ),
+                    labelText: "Número da Conta", hintText: "0000"),
                 keyboardType: TextInputType.number,
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
-                style: TextStyle(
-                  fontSize: 24.0
-                ),
+                controller: _controllerFieldValue,
+                style: TextStyle(fontSize: 24.0),
                 decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
+                    icon: Icon(Icons.monetization_on),
                     labelText: "Valor",
-                  hintText: "0.00"
-                ),
+                    hintText: "0.00"),
                 keyboardType: TextInputType.number,
               ),
             ),
-
             RaisedButton(
               child: Text("Confirmar"),
+              onPressed: () {
+                debugPrint("Clicou no Confimar");
+
+                final int numberAccount =
+                    int.tryParse(_controllerFieldNumberAccount.text);
+                final double value =
+                    double.tryParse(_controllerFieldValue.text);
+
+                if (numberAccount != null && value != null) {
+                  final transferCreated = Transferencia(value, numberAccount);
+                  debugPrint("$transferCreated");
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('$transferCreated'),
+                    ),
+                  );
+                }
+              },
             )
           ],
         ));
@@ -64,13 +77,13 @@ class ListaTranferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(1000, 1000)),
-        ItemTransferencia(Transferencia(200.0, 2000)),
-        ItemTransferencia(Transferencia(300.0, 3000)),
-      ],
-    ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(1000, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
@@ -87,10 +100,10 @@ class ItemTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text(_transferencia.valor.toString()),
-          subtitle: Text(_transferencia.numConta.toString()),
-        ));
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numConta.toString()),
+    ));
   }
 }
 
@@ -99,4 +112,9 @@ class Transferencia {
   final int numConta;
 
   Transferencia(this.valor, this.numConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numConta: $numConta}';
+  }
 }
