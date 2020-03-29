1f18b6036e3d7c5572e2861a7e40da2430ef0c86
Paloma <palomacorreiadasilva@gmail.com>
2020-03-29 19:22:46 -0300

    create Form transfers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() =>
@@ -19,10 +20,43 @@ class FormularioTransferencias  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Criando Transferências"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Criando Transferências"),),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  labelText: "Número da Conta",
+                  hintText: "0000"
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                    labelText: "Valor",
+                  hintText: "0.00"
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+
+            RaisedButton(
+              child: Text("Confirmar"),
+            )
+          ],
+        ));
   }
 }
 
